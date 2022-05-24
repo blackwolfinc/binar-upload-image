0141987e48b73e2aeafb417ec661b0bc0388298b
Black Wolf Team <47728533+blackwolfinc@users.noreply.github.com>
2022-05-24 21:04:25 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  // data value
+const [File, setFile] = useState(null)
+const [FilePrev, setFilePrev] = useState(null)
+
+
+const onChangePicture = e => {
+
+   
+
+  // mengecek adakah file apa tidak 
+  if (e.target.files[0]) {
+    // set file yang sudah ada kesalam use State
+    setFile(e.target.files[0]);
+    //  inisiai untuk merender data file yang sudah di upload
+    const reader = new FileReader();
+    // melakukan proses render dan di simpan dalam value
+    reader.addEventListener("load", () => {
+      setFilePrev(reader.result);
+    });
+    // melakuan render berdasrakan image yang di pilih
+    reader.readAsDataURL(e.target.files[0]);
+  }
+};
+
+
+// fungsi menggiram gambar
+const postToApi =()=>{
+  const FileHandle = File
+  var FormData = require('form-data');
+  var data = new FormData();
+  data.append('name', 'inovarr');
+  data.append('category', 'small');
+  data.append('price', '100000');
+  data.append('status', 'true');
+  data.append('image', FileHandle);
+  
+  var config = {
+    method: 'post',
+    url: 'https://rent-cars-api.herokuapp.com/admin/car',
+    data : data
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log(response.data);
+    console.log(response);
+   
+      alert(response.statusText)
+    
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
+
+
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ini input file </h1>
+      <img width={100} height={100} src={FilePrev}/>
+      <input onChange={(e)=>{onChangePicture(e)}} type="file"/>
+      <button onClick={()=>{postToApi()}}>Upload</button>
     </div>
   );
 }
